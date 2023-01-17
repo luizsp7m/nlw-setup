a66ab316a6d0de8a891d0d447575ae28c40e604b
Luiz Oliveira <luizoliveira2808@gmail.com>
2023-01-16 22:21:30 -0300

    Aula 01

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-900">
+      <h1 className="text-3xl font-bold underline text-white">
+        Hello World!
+      </h1>
+    </div>
+  );
+}
