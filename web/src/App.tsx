3ae3b2040fa9f1ce729f58943a035c0487fb121c
Luiz Oliveira <luizoliveira2808@gmail.com>
2023-01-17 21:35:50 -0300

    Aula 02

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,15 @@
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
+
+// import { Habit } from "./components/Habit"
+
 export function App() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-900">
-      <h1 className="text-3xl font-bold underline text-white">
-        Hello World!
-      </h1>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
-  );
+  )
 }
